6d97cd0fdc37edaabcb8399969bfaf9a9b65c96d
Artem <artemisia8192@gmail.com>
2024-09-14 21:20:45 +0200

    Added password strength validation

--- src/components/SignUpButton.tsx
+++ src/components/SignUpButton.tsx
@@ -10,12 +10,15 @@ import {
 	DialogClose,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
+import { Progress } from '@/components/ui/progress'
 
 import { useRef, useState, ReactElement } from 'react'
 import { useMutation, gql } from '@apollo/client'
 
 import { LoaderIcon } from 'lucide-react'
 import hasAtLeastNYears from '@/lib/hasAtLeastNYears'
+
+import zxcvbn from 'zxcvbn'
 
 
 const signUpMutation = gql`
@@ -33,15 +36,26 @@ export default function SignUpButton() {
 	const [signUp, signUpResponse] = useMutation(signUpMutation)
 	const [errorMessage, setErrorMessage] = useState<string | null>(null)
 	const usernameInputRef = useRef<HTMLInputElement | null>(null)
-	const passwordInputRef = useRef<HTMLInputElement | null>(null)
 	const secondPasswordInputRef = useRef<HTMLInputElement | null>(null)
 	const birthDateInputRef = useRef<HTMLInputElement | null>(null)
+	const [passwordValue, setPasswordValue] = useState('')
+	const [passwordStrength, setPasswordStrength] = useState<any>(null)
+
+	const onPasswordValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const password = event.target.value
+		setPasswordValue(password)
+		setPasswordStrength(zxcvbn(password))
+	}
 
 	const onSubmit = () => {
 		const username = usernameInputRef.current?.value ?? ''
-		const password = passwordInputRef.current?.value ?? ''
+		const password = passwordValue
 		const secondPassword = secondPasswordInputRef.current?.value ?? ''
 		const birthDate = new Date(birthDateInputRef.current?.value ?? '')
+		if (!passwordStrength || passwordStrength.score !== 4) {
+			setErrorMessage('Password is too easy to hack. Indicator must be green.')
+			return
+		}
 		if (!birthDate) {
 			setErrorMessage('Incorrect birth date.')
 			return
@@ -127,7 +141,19 @@ export default function SignUpButton() {
 				</DialogHeader>
 				<div className="flex flex-col gap-2">
 					<Input placeholder="Username" ref={usernameInputRef} />
-					<Input placeholder="Password" type="password" ref={passwordInputRef} />
+					<Input placeholder="Password" type="password" onChange={onPasswordValueChange} value={passwordValue} />
+					{passwordStrength && (
+							<div className="flex flex-col items-center gap-1 my-1">
+								<Progress value={passwordStrength.score * 25} className={['*:bg-red-900', '*:bg-red-600', '*:bg-yellow-600', '*:bg-teal-600', '*:bg-green-600'][passwordStrength.score] + ' w-[95%]'} />
+								{passwordStrength.feedback.warning && (
+										<p className="text-sm text-destructive-foreground">
+											{passwordStrength.feedback.warning}
+										</p>
+									)
+								}
+							</div>
+						)
+					}
 					<Input placeholder="Password (again)" type="password" ref={secondPasswordInputRef} />
 					<Input placeholder="Your birth date" type="date" ref={birthDateInputRef} />
 				</div>
